939b3278c8884d9ddc4357afa42b3f0ec9e31f02
yurim <y1551463@gmail.com>
2025-04-09 19:23:30 +0900

    빈 이름으로 조회

--- sq-ch2-ex1/src/main/java/org/example/Main.java
+++ sq-ch2-ex1/src/main/java/org/example/Main.java
@@ -10,7 +10,8 @@ public class Main {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class); // 스프링 컨텍스트 인스턴스 생성
 
         // 빈 타입이 동일하기 때문에 예외가 발생함
-        Parrot p = context.getBean(Parrot.class); // 스프링 컨텍스트에서 Parrot 타입의 빈 참조 조회
+        // Parrot p = context.getBean(Parrot.class); // 스프링 컨텍스트에서 Parrot 타입의 빈 참조 조회
+        Parrot p = context.getBean("parrot2", Parrot.class);
         System.out.println(p.getName());
     }
 }
