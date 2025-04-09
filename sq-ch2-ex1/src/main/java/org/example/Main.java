a9d81f5ddccdce263b3dd7bb90adbdd7af54942e
yurim <y1551463@gmail.com>
2025-04-09 19:26:09 +0900

    빈 이름으로 조회
    name 속성으로 빈 이름을 지정할 수 있다

--- sq-ch2-ex1/src/main/java/org/example/Main.java
+++ sq-ch2-ex1/src/main/java/org/example/Main.java
@@ -11,7 +11,7 @@ public class Main {
 
         // 빈 타입이 동일하기 때문에 예외가 발생함
         // Parrot p = context.getBean(Parrot.class); // 스프링 컨텍스트에서 Parrot 타입의 빈 참조 조회
-        Parrot p = context.getBean("parrot2", Parrot.class);
+        Parrot p = context.getBean("Miki", Parrot.class);
         System.out.println(p.getName());
     }
 }
--- sq-ch2-ex1/src/main/java/org/example/config/ProjectConfig.java
+++ sq-ch2-ex1/src/main/java/org/example/config/ProjectConfig.java
@@ -7,21 +7,21 @@ import org.springframework.context.annotation.Configuration;
 @Configuration // 스프링 구성 클래스 정의
 public class ProjectConfig {
 
-    @Bean
+    @Bean(name = "Koko")
     public Parrot parrot1() {
         var p = new Parrot();
         p.setName("Koko");
         return p;
     }
 
-    @Bean
+    @Bean(name = "Miki")
     public Parrot parrot2() {
         var p = new Parrot();
         p.setName("Miki");
         return p;
     }
 
-    @Bean
+    @Bean(name = "Riki")
     public Parrot parrot3() {
         var p = new Parrot();
         p.setName("Riki");
