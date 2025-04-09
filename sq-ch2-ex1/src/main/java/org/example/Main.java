46db8152d2eec8c697fc16b84f9cefec38ea5a59
yurim <y1551463@gmail.com>
2025-04-09 19:02:36 +0900

    스프링 컨텍스트 인스턴스 생성

--- sq-ch2-ex1/src/main/java/org/example/Main.java
+++ sq-ch2-ex1/src/main/java/org/example/Main.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class Main {
 
     public static void main(String[] args) {
+        
+        var context = new AnnotationConfigApplicationContext(); // 스프링 컨텍스트 인스턴스 생성
+        
         Parrot p = new Parrot();
     }
 }
