b02cff7dff5686f2c8eeb56cbecdd4315ef6ea57
yurim <y1551463@gmail.com>
2025-04-09 21:03:11 +0900

    registerBean() 메서드 사용하여 빈 추가 로직 재정의

--- sq-ch2-ex1/src/main/java/org/example/main/Main.java
+++ sq-ch2-ex1/src/main/java/org/example/main/Main.java
@@ -1,5 +1,6 @@
 package org.example.main;
 
+import java.util.function.Supplier;
 import org.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -9,7 +10,13 @@ public class Main {
         
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class); // 스프링 컨텍스트 인스턴스 생성
 
-        Parrot p = context.getBean(Parrot.class);
+        Parrot2 x = new Parrot2(); // 스프링 컨텍스트에 추가하고 싶은 인스턴스 생성
+        x.setName("Kiki");
+
+        Supplier<Parrot2> parrot2Supplier = () -> x;
+
+        context.registerBean("parrot1", Parrot2.class, parrot2Supplier); // 해당 인스턴스를 컨텍스트에 추가
+        Parrot2 p = context.getBean(Parrot2.class);
         System.out.println(p);
         System.out.println(p.getName());
     }
---
+++ sq-ch2-ex1/src/main/java/org/example/main/Parrot2.java
@@ -0,0 +1,15 @@
+package org.example.main;
+
+public class Parrot2 {
+
+    private String name;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
