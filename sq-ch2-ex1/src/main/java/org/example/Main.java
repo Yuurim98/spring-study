78edcabd20576c4e6c322d87be36ab408404ef0b
yurim <y1551463@gmail.com>
2025-04-09 19:14:14 +0900

    구성 클래스 기반으로 스프링 컨텍스트 인스턴스 생성 및 컨텍스트에 추가된 빈 가져오기

--- sq-ch2-ex1/src/main/java/org/example/Main.java
+++ sq-ch2-ex1/src/main/java/org/example/Main.java
@@ -1,13 +1,15 @@
 package org.example;
 
+import org.example.config.ProjectConfig;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
         
-        var context = new AnnotationConfigApplicationContext(); // 스프링 컨텍스트 인스턴스 생성
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class); // 스프링 컨텍스트 인스턴스 생성
         
-        Parrot p = new Parrot();
+        Parrot p = context.getBean(Parrot.class); // 스프링 컨텍스트에서 Parrot 타입의 빈 참조 조회
+        System.out.println(p.getName());
     }
 }
