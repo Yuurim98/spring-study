e3c2ad8f03772afcffa77c647c0f516dc67aefad
yurim <y1551463@gmail.com>
2025-04-09 19:53:35 +0900

    스테레오타입 어노테이션 활용
    ComponentScan으로 위치를 알려야 함

--- sq-ch2-ex1/src/main/java/org/example/config/ProjectConfig.java
+++ sq-ch2-ex1/src/main/java/org/example/config/ProjectConfig.java
@@ -1,33 +1,11 @@
 package org.example.config;
 
-import org.example.Parrot;
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration // 스프링 구성 클래스 정의
+@ComponentScan(basePackages = "org.example.main")
 public class ProjectConfig {
 
-    @Bean(name = "Koko")
-    @Primary
-    public Parrot parrot1() {
-        var p = new Parrot();
-        p.setName("Koko");
-        return p;
-    }
-
-    @Bean(name = "Miki")
-    public Parrot parrot2() {
-        var p = new Parrot();
-        p.setName("Miki");
-        return p;
-    }
-
-    @Bean(name = "Riki")
-    public Parrot parrot3() {
-        var p = new Parrot();
-        p.setName("Riki");
-        return p;
-    }
 
 }
--- sq-ch2-ex1/src/main/java/org/example/main/Parrot.java
+++ sq-ch2-ex1/src/main/java/org/example/main/Parrot.java
@@ -1,5 +1,8 @@
 package org.example.main;
 
+import org.springframework.stereotype.Component;
+
+@Component // 스테레오타입 어노테이션
 public class Parrot {
 
     private String name;
