d5a8cf551da653859c4ebed6b915724f19922d47
yurim <y1551463@gmail.com>
2025-04-20 17:23:08 +0900

    config, main 추가
    Person 객체는 Parrot 객체를 참조하지만, ProjectConfig에서는 Parrot과 Person 객체가 각각 독립적으로 등록되어 있기 때문에, Person 객체는 Parrot 객체를 참조하지 못한다

---
+++ sq-ch3-ex1/src/main/java/org/example/Main.java
@@ -0,0 +1,22 @@
+package org.example;
+
+import org.example.config.ProjectConfig;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // ProjectConfig 를 기반으로 스프링 컨텍스트 인스턴스 생성
+        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
+
+        // context 에서 빈 참조를 얻음
+        Person person = context.getBean(Person.class);
+        Parrot parrot = context.getBean(Parrot.class);
+
+        System.out.println("Person name : " + person.getName());
+        System.out.println("Parrot name : " + parrot.getName());
+
+        System.out.println("Person parrot : " + person.getParrot()); // null
+
+    }
+}
---
+++ sq-ch3-ex1/src/main/java/org/example/config/ProjectConfig.java
@@ -0,0 +1,24 @@
+package org.example.config;
+
+import org.example.Parrot;
+import org.example.Person;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public Parrot parrot() {
+        Parrot p = new Parrot();
+        p.setName("Koko");
+        return p;
+    }
+
+    @Bean
+    public Person person() {
+        Person p = new Person();
+        p.setName("홍길동");
+        return p;
+    }
+}
