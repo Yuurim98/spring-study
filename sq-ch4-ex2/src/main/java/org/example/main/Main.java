fe80257179e687a469469543f471ca720b9374a1
yurim <y1551463@gmail.com>
2025-04-24 17:44:48 +0900

    스프링 DI

--- sq-ch4-ex2/src/main/java/org/example/main/Main.java
+++ sq-ch4-ex2/src/main/java/org/example/main/Main.java
@@ -1,20 +1,23 @@
 package org.example.main;
 
+import org.example.configuration.ProjectConfiguration;
 import org.example.model.Comment;
 import org.example.proxies.EmailCommentNotificationProxy;
 import org.example.repositories.DBCommentRepository;
 import org.example.services.CommentService;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
-        var commentRepository = new DBCommentRepository();
-        var commentNotificationProxy = new EmailCommentNotificationProxy();
-        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+
+        var context = new AnnotationConfigApplicationContext(ProjectConfiguration.class);
 
         var comment = new Comment();
-        comment.setAuthor("홍길동");
-        comment.setText("안녕");
+        comment.setAuthor("길동이");
+        comment.setText("안녕하세요");
+
+        var commentService = context.getBean(CommentService.class);
 
         commentService.publishComment(comment);
     }
