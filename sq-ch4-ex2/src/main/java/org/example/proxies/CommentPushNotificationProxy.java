ca9cba54f0e143e321b30021f3f03c604324ce19
yurim <y1551463@gmail.com>
2025-04-24 17:51:23 +0900

    동일한 타입의 빈이 하나 이상일 때 1) Qualifier 로 주입 받을 빈 명시

--- sq-ch4-ex2/src/main/java/org/example/proxies/CommentPushNotificationProxy.java
+++ sq-ch4-ex2/src/main/java/org/example/proxies/CommentPushNotificationProxy.java
@@ -1,11 +1,12 @@
 package org.example.proxies;
 
 import org.example.model.Comment;
-import org.springframework.context.annotation.Primary;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
-@Primary // 기본 빈 지정
+@Qualifier("PUSH")
+// @Primary // 기본 빈 지정
 public class CommentPushNotificationProxy implements CommentNotificationProxy{
 
     @Override
--- sq-ch4-ex2/src/main/java/org/example/services/CommentService.java
+++ sq-ch4-ex2/src/main/java/org/example/services/CommentService.java
@@ -3,6 +3,7 @@ package org.example.services;
 import org.example.model.Comment;
 import org.example.proxies.CommentNotificationProxy;
 import org.example.repositories.CommentRepository;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -12,7 +13,7 @@ public class CommentService {
     private final CommentNotificationProxy commentNotificationProxy;
 
     public CommentService(CommentRepository commentRepository,
-        CommentNotificationProxy commentNotificationProxy) {
+        @Qualifier("PUSH") CommentNotificationProxy commentNotificationProxy) {
         this.commentRepository = commentRepository;
         this.commentNotificationProxy = commentNotificationProxy;
     }
