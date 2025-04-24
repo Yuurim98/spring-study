102beeabd06c9fbe0127b9255ba786d620e7b066
yurim <y1551463@gmail.com>
2025-04-24 17:15:52 +0900

    인터페이스 활용 추상화 (스프링 X)

--- sq-ch4-ex1/src/main/java/org/example/main/Main.java
+++ sq-ch4-ex1/src/main/java/org/example/main/Main.java
@@ -1,8 +1,21 @@
 package org.example.main;
 
+import org.example.model.Comment;
+import org.example.proxies.EmailCommentNotificationProxy;
+import org.example.repositories.DBCommentRepository;
+import org.example.services.CommentService;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        var commentRepository = new DBCommentRepository();
+        var commentNotificationProxy = new EmailCommentNotificationProxy();
+        var commentService = new CommentService(commentRepository, commentNotificationProxy);
+
+        var comment = new Comment();
+        comment.setAuthor("홍길동");
+        comment.setText("안녕");
+
+        commentService.publishComment(comment);
     }
 }
