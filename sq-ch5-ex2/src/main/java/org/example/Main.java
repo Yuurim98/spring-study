c921f2a31fa6e8ea9f0bbf786915f0fe54118832
yurim <y1551463@gmail.com>
2025-04-25 22:45:45 +0900

    스프링 빈 지연 초기화

--- sq-ch5-ex2/src/main/java/org/example/Main.java
+++ sq-ch5-ex2/src/main/java/org/example/Main.java
@@ -1,12 +1,21 @@
 package org.example;
 
 import org.example.config.ProjectConfig;
+import org.example.services.CommentService;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
 
     public static void main(String[] args) {
        var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-       // CommentService 빈 사용 X -> CommentService 빈 생성 콘솔 출력
+
+        System.out.println("CommentService 참조 전");
+        var commentService = context.getBean(CommentService.class);
+        System.out.println("CommentService 참조 후");
+
+//        CommentService 참조 전
+//        CommentService 빈 생성
+//        CommentService 참조 후
+
     }
 }
--- sq-ch5-ex2/src/main/java/org/example/services/CommentService.java
+++ sq-ch5-ex2/src/main/java/org/example/services/CommentService.java
@@ -1,8 +1,10 @@
 package org.example.services;
 
+import org.springframework.context.annotation.Lazy;
 import org.springframework.stereotype.Service;
 
 @Service
+@Lazy // 빈을 처음 참조할 때 생성
 public class CommentService {
 
     public CommentService() {
