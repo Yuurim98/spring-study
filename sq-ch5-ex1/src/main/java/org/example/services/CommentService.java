9c146ef7698763366530abcffed4ceec1cbd6f11
yurim <y1551463@gmail.com>
2025-04-25 22:15:33 +0900

    final로 정의하여 불변 보장

--- sq-ch5-ex1/src/main/java/org/example/services/CommentService.java
+++ sq-ch5-ex1/src/main/java/org/example/services/CommentService.java
@@ -1,14 +1,16 @@
 package org.example.services;
 
 import org.example.repositories.CommentRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CommentService {
 
-    @Autowired
-    private CommentRepository commentRepository;
+    private final CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
 
     public CommentRepository getCommentRepository() {
         return commentRepository;
