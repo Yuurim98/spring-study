19492e0df5273cc330b4871ab9805e874cb7171a
yurim <y1551463@gmail.com>
2025-04-28 18:10:46 +0900

    @AfterReturning 활용

--- sq-ch6-ex1/src/main/java/org/example/aspects/LoggingAspect.java
+++ sq-ch6-ex1/src/main/java/org/example/aspects/LoggingAspect.java
@@ -3,6 +3,7 @@ package org.example.aspects;
 import java.util.Arrays;
 import java.util.logging.Logger;
 import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
@@ -22,16 +23,9 @@ public class LoggingAspect {
 //        return returnByMethod;
 //    }
 
-    @Around("@annotation(ToLog)")
-    public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-        String methodName = joinPoint.getSignature().getName(); // 메서드 이름
-        Object[] arguments = joinPoint.getArgs(); // 매개변수 이름
-        logger.info("메서드명 " + methodName + "매개변수 " + Arrays.asList(arguments));
-        Object returnByMethod = joinPoint.proceed(); // 메서드 호출
-        logger.info("메서드 반환 " + returnByMethod);
-
-        returnByMethod += " 반환 값 수정";
-        return returnByMethod;
+    @AfterReturning(value = "@annotation(ToLog)", returning = "returnedValue")
+    public void log(Object returnedValue) { // 메서드 반환 값을 매개변수로 받는다
+        logger.info("메서드 정상 성공 후 반환 :" + returnedValue);
     }
 
 }
