97f92ba08d7a1727e7663e1a70b8b89182c61da2
yurim <y1551463@gmail.com>
2025-05-01 15:40:34 +0900

    request parameter 예제

--- sq-ch8-ex1/src/main/java/org/example/sqch8ex1/controllers/MainController.java
+++ sq-ch8-ex1/src/main/java/org/example/sqch8ex1/controllers/MainController.java
@@ -3,6 +3,7 @@ package org.example.sqch8ex1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
@@ -11,6 +12,13 @@ public class MainController {
     public String home(Model page) {
         page.addAttribute("username", "홍길동");
         page.addAttribute("color", "red");
+        return "home.html";
+    }
+
+    @RequestMapping("/home2")
+    public String home2(@RequestParam(required = false) String color, @RequestParam String name, Model page) {
+        page.addAttribute("username", name);
+        page.addAttribute("color", color);
         return "home.html";
     }
 
