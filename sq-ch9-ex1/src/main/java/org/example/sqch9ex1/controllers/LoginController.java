7ec6f5b80f6582af2f64b5873e04b4f919892485
yurim <y1551463@gmail.com>
2025-06-03 20:04:42 +0900

    요청 스코프
    - 각 HTTP 요청마다 인스턴스를 새로 생성한다
    - 생성된 인스턴스는 해당 HTTP 요청이 끝나면 소멸된다
    - LoginProcessor를 요청 스코프로 지정하지 않으면 스프링은 생성해둔 인스턴스 하나를 HTTP 요청마다 재사용한다 (싱글톤) (로그인 요청 시 count가 계속 증가함)
    - `@RequestScope` 를 지정하여 HTTP 요청마다 인스턴스를 새로 생성한다 (로그인 요청 시 count 올라가지 않음)

---
+++ sq-ch9-ex1/src/main/java/org/example/sqch9ex1/controllers/LoginController.java
@@ -0,0 +1,38 @@
+package org.example.sqch9ex1.controllers;
+
+import org.example.sqch9ex1.model.LoginProcessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class LoginController {
+
+    private final LoginProcessor loginProcessor;
+
+    public LoginController(LoginProcessor loginProcessor) {
+        this.loginProcessor = loginProcessor;
+    }
+
+    @GetMapping("/")
+    public String loginGet() {
+        return "login.html";
+    }
+
+    @PostMapping("/")
+    public String loginPost(@RequestParam String username, @RequestParam String password, Model model) {
+
+        loginProcessor.setUsername(username);
+        loginProcessor.setPassword(password);
+        boolean loggedIn = loginProcessor.login();
+
+        if (loggedIn) {
+            model.addAttribute("message", "로그인 성공했습니다.");
+        } else {
+            model.addAttribute("message", "로그인 실패했습니다.");
+        }
+        return "login.html";
+    }
+}
---
+++ sq-ch9-ex1/src/main/java/org/example/sqch9ex1/model/LoginProcessor.java
@@ -0,0 +1,43 @@
+package org.example.sqch9ex1.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.annotation.RequestScope;
+
+@Component
+@RequestScope
+public class LoginProcessor {
+
+    private String username;
+    private String password;
+    private int count;
+
+    public boolean login() {
+        String username = this.getUsername();
+        String password = this.getPassword();
+        count++;
+
+        System.out.println("요청 : " + count);
+
+        if ("test".equals(username) && "password".equals(password)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
