2efec36eeef5c3de4fb8ca5cc707263f4057a9e0
yurim <y1551463@gmail.com>
2025-06-04 17:30:40 +0900

    ResponseEntity 반환

--- sq-ch10-ex2/src/main/java/org/example/sqch10ex2/controllers/CountryController.java
+++ sq-ch10-ex2/src/main/java/org/example/sqch10ex2/controllers/CountryController.java
@@ -2,6 +2,8 @@ package org.example.sqch10ex2.controllers;
 
 import java.util.List;
 import org.example.sqch10ex2.model.Country;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class CountryController {
 
     @GetMapping("/france")
-    public Country france() {
+    public ResponseEntity<Country> france() {
         Country c = Country.of("France", 67);
-        return c;
+        return ResponseEntity
+            .status(HttpStatus.ACCEPTED)
+            .header("header", "hi")
+            .body(c);
     }
 
     @GetMapping("/all")
