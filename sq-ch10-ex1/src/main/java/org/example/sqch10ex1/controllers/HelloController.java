eeea6224065f697a3bc8331684f8c36d5a391825
yurim <y1551463@gmail.com>
2025-06-04 17:15:34 +0900

    rest 엔드포인트
    Controller + ResponseBody

--- sq-ch10-ex1/src/main/java/org/example/sqch10ex1/controllers/HelloController.java
+++ sq-ch10-ex1/src/main/java/org/example/sqch10ex1/controllers/HelloController.java
@@ -3,12 +3,14 @@ package org.example.sqch10ex1.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+// @Controller
+@RestController // Controller + ResponseBody
 public class HelloController {
 
     @GetMapping("/hello")
-    @ResponseBody // HTTP 응답 직접 반환을 나타낸다
+    // @ResponseBody // HTTP 응답 직접 반환을 나타낸다
     public String hello() {
         return "hello";
     }
