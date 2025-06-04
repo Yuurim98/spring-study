0ca9bb19534097b21a5bb0120939cfd4fc801929
yurim <y1551463@gmail.com>
2025-06-04 17:49:59 +0900

    sq-ch10-ex4

---
+++ sq-ch10-ex4/src/test/java/org/example/sqch10ex4/SqCh10Ex4ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.sqch10ex4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqCh10Ex4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
